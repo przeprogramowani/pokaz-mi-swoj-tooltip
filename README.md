23a9d091bd2f0400f54e46c967123bd4e782c600
przeprogramowani <przeprogramowani@gmail.com>
2020-03-01 19:29:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pokaz-mi-swoj-tooltip
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/pokaz-mi-swoj-tooltip)
---
+++ index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+
+const members = [
+  { first_name: 'Adam', last_name: 'Gospodarczyk', role: 'Member'},
+  { first_name: 'Przemek', last_name: 'Smyrdek', role: 'Member'},
+  { first_name: 'Marcin', last_name: 'Czarkowski', role: 'Member'}
+];
+
+new Vue({
+  el: '#app',
+  data: {
+
+  }
+})
